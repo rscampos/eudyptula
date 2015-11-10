46436ac3ed26f02629fac3c999c5b04abc8c7420
Raphael Campos <raphaelcampos.rp@gmail.com>
2015-11-10 20:19:01 -0200

    debugfs cleanup

--- task08/debugfs.c
+++ task08/debugfs.c
@@ -54,32 +54,11 @@ ssize_t device_write_id(struct file *filp,
 ssize_t device_read_jiffies(struct file *filp,
                     char *bufStoreData, size_t bufCount, loff_t *curOffset)
 {
-    int ret;
-    unsigned long temp = jiffies;
+        int ret;
+        unsigned long temp = jiffies;
 
-    pr_info("----------------------------------------\n");
-    pr_info("[Jiffies :0x%x %d]\n", temp, sizeof(temp));
-
-    /* Buffer before */
-    pr_info("[Buffer user (before): &0x%x[0x%x]]\n", &bufStoreData, bufStoreData);
-    pr_info("    &0x%x[0x%x%x%x%x]\n", bufStoreData, *(bufStoreData+3),
-                    *(bufStoreData+2),*(bufStoreData+1), *(bufStoreData));
-
-    pr_info("[Writing: (0x%x) [size:%d] to 0x%x\n", temp, sizeof(temp), bufStoreData);
-
-    pr_info("[Simple_read_from_buffer(0x%x, %d, 0x%x, 0x%x, %d)]\n", bufStoreData,
-                    bufCount, curOffset, &temp, sizeof(temp));
-
-    ret = simple_read_from_buffer(bufStoreData, bufCount, curOffset, &temp, sizeof(temp));
-
-    /* Buffer after */
-    pr_info("[Buffer user (after): &0x%x[0x%x]]\n", &bufStoreData, bufStoreData);
-    pr_info("    &0x%x[0x%x%x%x%x]\n", bufStoreData, *(bufStoreData+3),
-                    *(bufStoreData+2),*(bufStoreData+1), *(bufStoreData));
-
-    pr_info("----------------------------------------\n");
-
-    return ret;
+        return simple_read_from_buffer(bufStoreData, bufCount, curOffset,
+                                       &temp, sizeof(temp));
 }
 
 /* Handles for the file 'foo' */
@@ -87,21 +66,11 @@ ssize_t device_read_foo(struct file *filp,
                     char *bufStoreData, size_t bufCount, loff_t *curOffset)
 {
         int ret;
-        int i;
-
-        pr_info("[READ][BEFORE  ][Alloc:&0x%x[0x%x]=0x%x]\n", &block, block, *block);
-        pr_info("[READ][BEFORE  ][LOCK: &0x%x[0x%x]]\n", &rw_sem, rw_sem);
 
         down_read(&rw_sem);
-        pr_info("      [READ][INSIDE  ][LOCK: &0x%x[0x%x]]\n", &rw_sem, rw_sem);
-        ret = simple_read_from_buffer(bufStoreData, bufCount, curOffset, block, PAGE_SIZE);
+        ret = simple_read_from_buffer(bufStoreData, bufCount, curOffset, block,
+                                      PAGE_SIZE);
         up_read(&rw_sem);
-
-        pr_info("[READ][AFTER   ][LOCK: &0x%x[0x%x]]\n", &rw_sem, rw_sem);
-        pr_info("[READ][AFTER   ]:");
-
-        for(i=0; i < 10; i++)
-            pr_info("       block - [0x%x -  0x%x] - bufStoreData\n",block[i],bufStoreData[i]);
 
         return ret;
 }
@@ -111,32 +80,23 @@ ssize_t device_write_foo(struct file *filp,
                      loff_t *curOffset)
 {
         int ret;
-        int i;
 
         if (bufCount > PAGE_SIZE - 1)
                 return -EINVAL;
 
-        if(!block){
+        if (!block){
             block = kmalloc(PAGE_SIZE, GFP_KERNEL);
 
             if (!block)
                 return -ENOMEM;
         }
 
-        pr_info("[WRITE][BEFORE][Alloc: &0x%x[0x%x]=0x%x]\n", &block, block, *block);
-        pr_info("[WRITE][BEFORE][LOCK:  &0x%x[0x%x]]\n", &rw_sem, rw_sem);
-
         down_write(&rw_sem);
-        pr_info("      [WRITE][INSIDE  ][LOCK: &0x%x[0x%x]]\n", &rw_sem, rw_sem);
         ret = simple_write_to_buffer(block, bufCount, curOffset, bufSourceData,
                                      PAGE_SIZE);
         up_write(&rw_sem);
 
         block[bufCount] = '\0';
-
-        pr_info("[WRITE][AFTER][LOCK:   &0x%x[0x%x]]\n", &rw_sem, rw_sem);
-        pr_info("[WRITE][AFTER][Alloc:  &0x%x[0x%x]=0x%x]\n", &block, block, *block);
-        pr_info("[WRITE][STRIN][Alloc:  [%s]\n", block);
 
         return ret;
 }
@@ -159,9 +119,9 @@ const struct file_operations fops_foo = {
 static int driver_entry(void)
 {
         struct dentry *debugfs_file;
-        umode_t mode_id = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
-        umode_t mode_jiffies = S_IRUSR|S_IRGRP|S_IROTH;
-        umode_t mode_foo = S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH;
+        umode_t mode_id     = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
+        umode_t mode_jiffies= S_IRUSR|S_IRGRP|S_IROTH;
+        umode_t mode_foo    = S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH;
         unsigned int data;
 
         init_rwsem(&rw_sem);
@@ -189,21 +149,15 @@ static int driver_entry(void)
         if (!debugfs_file)
                 return ERR_PTR(-ENOMEM);
 
-
-        pr_info("driver_entry!\n");
         return 0;
 }
 
-static int driver_exit(void)
+static void driver_exit(void)
 {
         debugfs_remove_recursive(debugfs_dir);
 
         if(!block)
-            kfree(block);
-
-        pr_info("driver_exit!\n");
-
-        return 0;
+                kfree(block);
 }
 
 module_init(driver_entry);
