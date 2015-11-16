822b6adb831c053679c888745c507860f66d5a61
Raphael Campos <raphaelcampos.rp@gmail.com>
2015-11-16 01:32:30 -0200

    Task 08: add jiffies file

--- task08/debugfs.c
+++ task08/debugfs.c
@@ -11,6 +11,7 @@
 static const char dir_name[] = "eudyptula";
 static const char file_name_id[] = "id";
 static const char euid[] = "4f92b3aee379";
+static const char file_name_jiffies[] = "jiffies";
 static struct dentry *debugfs_dir;
 
 /* Handles for the file 'id' */
@@ -42,15 +43,32 @@ ssize_t device_write_id(struct file *filp,
 	return ret;
 }
 
+/* Handle for the file 'jiffies' */
+ssize_t device_read_jiffies(struct file *filp,
+			    char *bufStoreData, size_t bufCount,
+			    loff_t *curOffset)
+{
+	int ret;
+	unsigned long temp = jiffies;
+
+	return simple_read_from_buffer(bufStoreData, bufCount, curOffset,
+				       &temp, sizeof(temp));
+}
+
 const struct file_operations fops_id = {
 	.write	= device_write_id,
 	.read	= device_read_id
+};
+
+const struct file_operations fops_jiffies = {
+	.read	= device_read_jiffies
 };
 
 static int driver_entry(void)
 {
 	struct dentry *debugfs_file;
 	umode_t mode_id = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
+	umode_t mode_jiffies = S_IRUSR|S_IRGRP|S_IROTH;
 	unsigned int data;
 
 	debugfs_dir = debugfs_create_dir(dir_name, NULL);
@@ -61,6 +79,12 @@ static int driver_entry(void)
 	/* id file */
 	debugfs_file = debugfs_create_file(file_name_id, mode_id,
 					   debugfs_dir, &data, &fops_id);
+	if (!debugfs_file)
+		return ERR_PTR(-ENOMEM);
+
+	/* jiffies file */
+	debugfs_file = debugfs_create_file(file_name_jiffies, mode_jiffies,
+					   debugfs_dir, &data, &fops_jiffies);
 	if (!debugfs_file)
 		return ERR_PTR(-ENOMEM);
 
