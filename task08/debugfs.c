bb06309a124368e6475f03e44c5e03290baae5c3
Raphael Campos <raphaelcampos.rp@gmail.com>
2015-11-16 02:03:33 -0200

    Task 08: add foo file

--- task08/debugfs.c
+++ task08/debugfs.c
@@ -7,12 +7,18 @@
 #include <linux/init.h>
 #include <linux/debugfs.h>
 #include <linux/jiffies.h>
+#include <linux/slab.h>
+#include <linux/gfp.h>
+#include <linux/rwsem.h>
 
 #define BUF_LEN 13
 static const char dir_name[] = "eudyptula";
 static const char file_name_id[] = "id";
 static const char euid[] = "4f92b3aee379";
 static const char file_name_jiffies[] = "jiffies";
+static const char file_name_foo[] = "foo";
+static unsigned char *block;
+struct rw_semaphore rw_sem;
 static struct dentry *debugfs_dir;
 
 /* Handles for the file 'id' */
@@ -55,6 +61,46 @@ ssize_t device_read_jiffies(struct file *filp, char *bufStoreData,
 				       &temp, sizeof(temp));
 }
 
+/* Handles for the file 'foo' */
+ssize_t device_read_foo(struct file *filp, char *bufStoreData,
+			size_t bufCount, loff_t *curOffset)
+{
+	int ret;
+
+	down_read(&rw_sem);
+	ret = simple_read_from_buffer(bufStoreData, bufCount, curOffset, block,
+				      PAGE_SIZE);
+	up_read(&rw_sem);
+
+	return ret;
+}
+
+ssize_t device_write_foo(struct file *filp, const char *bufSourceData,
+			 size_t bufCount, loff_t *curOffset)
+{
+	int ret;
+
+	if (bufCount > PAGE_SIZE - 1)
+		return -EINVAL;
+
+	if (!block) {
+		block = kmalloc(PAGE_SIZE, GFP_KERNEL);
+
+		if (!block)
+			return -ENOMEM;
+	}
+
+	down_write(&rw_sem);
+
+	ret = simple_write_to_buffer(block, bufCount, curOffset, bufSourceData,
+				     PAGE_SIZE);
+	up_write(&rw_sem);
+
+	block[bufCount] = '\0';
+
+	return ret;
+}
+
 const struct file_operations fops_id = {
 	.write	= device_write_id,
 	.read	= device_read_id
@@ -64,12 +110,20 @@ const struct file_operations fops_jiffies = {
 	.read   = device_read_jiffies
 };
 
+const struct file_operations fops_foo = {
+	.write  = device_write_foo,
+	.read   = device_read_foo
+};
+
 static int driver_entry(void)
 {
 	struct dentry *debugfs_file;
 	umode_t mode_id = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
 	umode_t mode_jiffies = S_IRUSR|S_IRGRP|S_IROTH;
+	umode_t mode_foo = S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH;
 	unsigned int data;
+
+	init_rwsem(&rw_sem);
 
 	debugfs_dir = debugfs_create_dir(dir_name, NULL);
 
@@ -88,12 +142,21 @@ static int driver_entry(void)
 	if (!debugfs_file)
 		return ERR_PTR(-ENOMEM);
 
+	/* foo file */
+	debugfs_file = debugfs_create_file(file_name_foo, mode_foo,
+					   debugfs_dir, &data, &fops_foo);
+	if (!debugfs_file)
+		return ERR_PTR(-ENOMEM);
+
 	return 0;
 }
 
 static void driver_exit(void)
 {
 	debugfs_remove_recursive(debugfs_dir);
+
+	if (!block)
+		kfree(block);
 }
 
 module_init(driver_entry);
